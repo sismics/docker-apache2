dfd75c46bd9f8f0f898df0306c2c6d060a3cbd6c
Jean-Marc Tremeaux <jm.tremeaux@sismics.com>
2016-12-30 12:26:47 +0100

    Make logs available on console

--- opt/bin/start-apache.sh
+++ opt/bin/start-apache.sh
@@ -2,4 +2,5 @@
 
 # Prevents "httpd (pid 1) already running" error when restarting the container
 rm -f $APACHE_PID_FILE
-/usr/sbin/apache2 -D FOREGROUND
+/usr/sbin/apache2 &
+tail -f /var/log/apache2/*
